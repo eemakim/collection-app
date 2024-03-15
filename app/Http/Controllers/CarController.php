f79ae62367709c29fbf77c97fc5d78c81405c033
Emma Kim <eema.kim01@gmail.com>
2024-03-15 15:12:46 +0000

    adding update car method

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -79,4 +79,50 @@ class CarController extends Controller
             'message' => 'New car added',
         ]);
     }
+
+    public function update(int $id, Request $request)
+    {
+        $request->validate([
+            'make' => 'string|max:20',
+            'model' => 'string|max:50',
+            'year' => 'integer|min:1950|max:2024',
+            'number_plate' => 'string|max:7|uppercase',
+            'weight' => 'nullable|integer|max:3000',
+            'electric' => 'boolean',
+        ]);
+        $car = Car::find($id);
+        if (! $car) {
+            return response()->json([
+                'message' => 'Car not found',
+            ], 400);
+        }
+
+        if ($request->make) {
+            $car->make = $request->make;
+        }
+        if ($request->model) {
+            $car->model = $request->model;
+        }
+        if ($request->year) {
+            $car->year = $request->year;
+        }
+        if ($request->number_plate) {
+            $car->number_plate = $request->number_plate;
+        }
+        if ($request->weight) {
+            $car->weight = $request->weight;
+        }
+        if ($request->electric) {
+            $car->electric = $request->electric;
+        }
+        if (! $car->save()) {
+            return response()->json([
+                'message' => 'Error - could not update car',
+            ], 500);
+        }
+
+        return response()->json([
+            'message' => 'Car updated successfully',
+        ]);
+    }
 }
